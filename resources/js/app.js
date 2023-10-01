c319ea170ae23ae5d13d688eda345f730c3fb0d5
hannaplsz <hanna.pleesz@gmail.com>
2023-10-01 14:36:42 +0200

    Import All to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+/*
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-
+*/
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,7 +20,9 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+/*
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+*/
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +30,19 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+/*
 const app = new Vue({
     el: '#app',
 });
+*/
+import { createApp } from 'vue'
+
+require('./bootstrap')
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+
+const app = createApp(App)
+app.config.globalProperties.$axios = axios;
+app.use(router)
+app.mount('#app')
