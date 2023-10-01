b550035224cb52bd87e336dc55ef35f26f6e1709
hannaplsz <hanna.pleesz@gmail.com>
2023-10-01 14:21:22 +0200

    Create Migration, Model and Controller

---
+++ app/Http/Controllers/API/SourceController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SourceController extends Controller
+{
+        // all sources
+        public function index()
+        {
+            $sources = Source::all()->toArray();
+            return array_reverse($sources);
+        }
+    
+        // add source
+        public function add(Request $request)
+        {
+            $book = new Source([
+                'source_name' => $request->source_name,
+                'api_key' => $request->api_key
+            ]);
+            $book->save();
+    
+            return response()->json('The source successfully added');
+        }
+    
+        // edit source
+        public function edit($id)
+        {
+            $source = Source::find($id);
+            return response()->json($source);
+        }
+    
+        // update source
+        public function update($id, Request $request)
+        {
+            $source = Source::find($id);
+            $source->update($request->all());
+    
+            return response()->json('The source successfully updated');
+        }
+    
+        // delete source
+        public function delete($id)
+        {
+            $source = Source::find($id);
+            $source->delete();
+    
+            return response()->json('The source successfully deleted');
+        }
+}
